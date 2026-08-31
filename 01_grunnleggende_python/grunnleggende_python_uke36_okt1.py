2f3c798ec1bb3e87764efaf682930f0a8b82debf
Kristofferto <kfaustnes@gmail.com>
2026-08-31 09:08:51 +0200

    Oppdatert kode

---
+++ 01_grunnleggende_python/grunnleggende_python_uke36_okt1.py
@@ -0,0 +1,28 @@
+print('hello world')
+a = 1
+print('Dette er et tall ', a)
+
+print('2026', '08', '31', sep='-')
+print(2 * 100)
+
+# dette er en kommentar
+# dette er en kommentar, hele linjen gjøres til en kommentar med ctrl + ' (stjerneknappen)
+
+# Variabler og datatyper
+navn = 'Kristoffer'
+alder = 29
+hoyde = 1.84
+er_kul = False
+
+print(navn, alder, hoyde, er_kul)
+print(type(navn), type(alder), type(hoyde), type(er_kul))
+
+alder = alder + 1
+# alder += 1
+
+print('42' + '1')
+print(42 + 1)
+print(alder == 30)
+print(navn == 'Kristoffe')
+
+# pause til 09.11
